3f3e75c11414f1291ad19ff915922d5a3722ce00
estherpinos <estherpinos1@gmail.com>
2023-11-06 16:53:53 +0100

    Add [stampare elementi dei films ]

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php 
 
+require_once __DIR__ ."/Model/Movie.php";
+require_once __DIR__ ."/Model/Media.php";
 
+$silencio = new Movie ('EL silencio', 'Clarice, affascinata da Hannibal, affronta con il suo aiuto un spietato assassino. Clarice Starling, giovane e ambiziosa studentessa dell\'Accademia dell\'FBI.', '8');
+$homealone = new Movie ('Home Alone','Kevin McCallister, il bambino protagonista, è l\'ultimo di cinque figli. I genitori pianificano un viaggio durante le feste natalizie coi figli ed altri parenti dimenticando però Kevin a casa per errore. ','9',) ;
+
+var_dump($silencio);
+var_dump($homealone);
 
 ?>
 
