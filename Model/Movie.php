94c33ce9e5dc764e7c490962a03922ccaaf2dafd
estherpinos <estherpinos1@gmail.com>
2023-11-06 16:12:16 +0100

    Edit [file movie(ad $image)]

--- Model/Movie.php
+++ Model/Movie.php
@@ -4,8 +4,18 @@ class Movie{
     public $movie_name;
     public $description_movie;
     public $vote;
+    public $image;
 
-    public function __construct($_movie_name, $_description_movie, $_img_movie){}
+    public function __construct( string $_movie_name, string $_description_movie, float $_vote)
+    {
+      $this->movie_name = $_movie_name;
+      $this->description_movie = $_description_movie;
+      $this->vote = $_vote;
+    }
+
+     public function setImage(Media $image){
+      $this->image = $image;
+     }
 }
 
 ?>
