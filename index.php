0655ea85d777f5b21d8b228723b2fb4f550e777c
estherpinos <estherpinos1@gmail.com>
2023-11-06 17:22:33 +0100

    Add [card bootrsap]

--- index.php
+++ index.php
@@ -18,6 +18,19 @@ require_once __DIR__ ."/db/db.php";
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
+
+<div class="container">
+
+  <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <p class="card-text">vote</p>
+      
+    </div>
+  </div>
+</div>
   
 </body>
 </html>
