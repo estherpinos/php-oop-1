44f8322129275bbcaae8497725be6557fabf9401
estherpinos <estherpinos1@gmail.com>
2023-11-07 15:25:58 +0100

    Edit [class tv serries]

--- Model/Movie.php
+++ Model/Movie.php
@@ -31,7 +31,7 @@ class Movie extends Production {
 }
 
 class TvSerie extends Production {
-  private $seasons;
+  public $seasons;
 
   public function __construct(string $_movie_name, string $_description_movie, float $_vote, Media $_image = null, $seasons) {
       parent::__construct($_movie_name, $_description_movie, $_vote, $_image);
