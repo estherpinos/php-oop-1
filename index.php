0f977c02739463d75aee52a3d8df535c2d911c76
estherpinos <estherpinos1@gmail.com>
2023-11-07 09:40:03 +0100

    Edit [color cards]

--- index.php
+++ index.php
@@ -17,15 +17,15 @@ require_once __DIR__ ."/db/db.php";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
-<body>
+<body >
 
-<div class="container mt-4 d-flex  ">
+<div class="container mt-5 d-flex  ">
 
   <?php
   foreach($movies as $movie):
   ?>
 
-    <div class="card mx-2 " style="width: 18rem;">
+    <div class="card mx-2 bg-light rounded-2 " style="width: 18rem;">
       <img class=" h-25 " src="img/<?php echo $movie->image->file_name ?>" class="card-img-top" alt="<?php echo $movie->image->name ?>">
       <div class="card-body" >
         <h5 class="card-title"><?php echo $movie->movie_name ?></h5>
