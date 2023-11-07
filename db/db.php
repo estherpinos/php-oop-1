64f18226a9784d6fe9512e78e534ca1f54ac5a81
estherpinos <estherpinos1@gmail.com>
2023-11-07 16:05:15 +0100

    Add [films e series e css]

--- db/db.php
+++ db/db.php
@@ -1,14 +1,29 @@
 <?php 
 
 $movies = [
-  new Movie ('El silencio', 'Clarice, affascinata da Hannibal, affronta con il suo aiuto un spietato assassino. Clarice Starling, giovane e ambiziosa studentessa dell\'Accademia dell\'FBI.', 8, new Media('elsilencio.jpg', 'El silencio')),
+  new Movie ('El silencio', 'Clarice, affascinata da Hannibal, affronta con il suo aiuto un spietato assassino. Clarice Starling, giovane e ambiziosa studentessa dell\'Accademia dell\'FBI.', 8, new Media('elsilencio.jpg', 'El silencio'), 130),
 
-  new Movie ('Home Alone','Kevin McCallister, il bambino protagonista, è l\'ultimo di cinque figli. I genitori pianificano un viaggio durante le feste natalizie coi figli ed altri parenti dimenticando però Kevin a casa per errore. ',9,  new Media('homealone.jpg', 'Home alone')) ,
+  new Movie ('Home Alone','Kevin McCallister, il bambino protagonista, è l\'ultimo di cinque figli. I genitori pianificano un viaggio durante le feste natalizie coi figli ed altri parenti dimenticando però Kevin a casa per errore. ',9,  new Media('homealone.jpg', 'Home alone'),120) ,
 
-  new Movie ('Il grinch','Cindy Lou è una bambina che vive nel paese di Chinonso e, in prossimità dei festeggiamenti per l\'arrivo del Natale, convince il sindaco del paesino ad invitare il Grinch, uno scontroso e malvagio essere fantastico, che vive con gli oggetti presi dalla discarica.',9, new Media('Grinch.jpg','il grinch' )),
+  new Movie ('Il grinch','Cindy Lou è una bambina che vive nel paese di Chinonso e, in prossimità dei festeggiamenti per l\'arrivo del Natale, convince il sindaco del paesino ad invitare il Grinch, uno scontroso e malvagio essere fantastico, che vive con gli oggetti presi dalla discarica.',9, new Media('Grinch.jpg','il grinch' ), 100),
 
-  new Movie ('Babbo Natale','Babbo Natale ha un grosso problema: tutti i bambini vogliono regali tecnologici, ma il suo team di elfi non è in grado di produrli. L\'elfo Romeo ha un altro problema: sua moglie è incinta ed entrambi sono preoccupati per il futuro della loro famigliola.',6, new Media('babbo.jpg', 'babbo natale')),
+  new Movie ('Babbo Natale','Babbo Natale ha un grosso problema: tutti i bambini vogliono regali tecnologici, ma il suo team di elfi non è in grado di produrli. L\'elfo Romeo ha un altro problema: sua moglie è incinta ed entrambi sono preoccupati per il futuro della loro famigliola.',6, new Media('babbo.jpg', 'babbo natale'),110),
 
 
 ];
+
+$series = [
+  new TvSerie ('La casa di carta', 'Un criminale ha un piano per realizzare la più grande rapina della storia, da realizzare alla Zecca Reale di Spagna. Per realizzare i propri scopi, l\'uomo recluta otto persone che non hanno nulla da perdere.', 9, new Media('casadicarta.jpg', ' casas di carta'), 5),
+
+  new TvSerie (' Vis a vis','Macarena Ferreiro è una giovane donna che, per amore del proprio capo, commette alcune gravi irregolarità fiscali e si ritrova imprigionata in un carcere femminile di massima sicurezza.',9,  new Media('visavis.jpg', 'Vis a vis'),4) ,
+
+  new TvSerie ('Castle','Rick Castle, un celebre scrittore di romanzi gialli in cerca di ispirazione, viene contattato dalla detective Kate Beckett del NYPD per aiutare le forze dell\'ordine a risolvere casi complessi',9, new Media('castle.jpg','castle' ), 8),
+
+  new TvSerie ('The mentalist','Patrick Jane, consulente del California Bureau of Investigation, utilizza le sue raffinate capacità di osservazione per risolvere i casi e per catturare l\'assassino della moglie e della figlia.
+  ',8, new Media('thementalist.jpg','mentalist' ), 7)
+
+
+];
+
+
 ?>
--- index.php
+++ index.php
@@ -16,8 +16,16 @@ require_once __DIR__ ."/db/db.php";
   <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+  <!-- css -->
+
+  <link rel="stylesheet" href="style.css">
 </head>
 <body >
+
+<!-- FILMS -->
+
+<h2 class="text-center mt-5 text-white">Films</h2>
 
 <div class="container mt-5 d-flex  ">
 
@@ -26,11 +34,36 @@ require_once __DIR__ ."/db/db.php";
   ?>
 
     <div class="card mx-2 bg-light rounded-2 " style="width: 18rem;">
-      <img class=" h-25 " src="img/<?php echo $movie->image->file_name ?>" class="card-img-top" alt="<?php echo $movie->image->name ?>">
+      <img src="img/<?php echo $movie->image->file_name ?>" class="card-img-top" alt="<?php echo $movie->image->name ?>">
       <div class="card-body" >
         <h5 class="card-title"><?php echo $movie->movie_name ?></h5>
-        <p class="card-text h-25 overflow-auto "><?php echo $movie->description_movie ?></p>
+        <p class="card-text overflow-auto description"><?php echo $movie->description_movie ?></p>
         <p class="card-text">Voto: <?php echo $movie->vote ?></p>
+        <p class="card-text">Durata: <?php echo $movie->duration ?> min</p>
+        
+      </div>
+    </div>
+
+  <?php 
+    endforeach;
+    ?>
+</div>
+
+<!-- SERIES -->
+<h2 class=" text-center mt-5 text-white  ">Series</h2>
+<div class="container mt-5 d-flex  ">
+
+  <?php
+  foreach($series as $TVseries):
+  ?>
+
+    <div class="card mx-2 bg-light rounded-2 " style="width: 18rem;">
+      <img src="img/<?php echo $TVseries->image->file_name ?>" class="card-img-top" alt="<?php echo $movie->image->name ?>">
+      <div class="card-body" >
+        <h5 class="card-title"><?php echo $TVseries->movie_name ?></h5>
+        <p class="card-text overflow-auto description "><?php echo $TVseries->description_movie ?></p>
+        <p class="card-text">Voto: <?php echo $TVseries->vote ?></p>
+        <p class="card-text">Stagioni: <?php echo $TVseries->seasons ?></p>
         
       </div>
     </div>
