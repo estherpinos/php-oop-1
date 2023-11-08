78709aa6b36319f2a22d5249e946a96d9d4b7448
estherpinos <estherpinos1@gmail.com>
2023-11-08 16:48:20 +0100

    fixed [ordine]

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 require_once __DIR__ ."/Model/Production.php";
 require_once __DIR__ ."/Model/Media.php";
-require_once __DIR__ ."/db/db.php";
 require_once __DIR__ ."/Model/Movie.php";
 require_once __DIR__ ."/Model/TvSerie.php";
+require_once __DIR__ ."/db/db.php";
 
 
 ?>
